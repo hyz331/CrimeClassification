eefeae5b131c70034fd522b1410c6fc45ff5e5b1
HeYunzhong <yunzhong@wifi-131-179-18-169.host.ucla.edu>
2015-11-25 10:57:08 -0800

    Add year, month, day and weekday into feature set Perform data normalization

--- oneHotEncoding.py
+++ oneHotEncoding.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.io as sio
+import json
 import sys
 import csv
 import time
@@ -16,20 +17,45 @@ csv = csv.reader(csvfile, delimiter=',')
 # One hot encoding
 Features = []
 Labels = []
-category_map = dict()
+label_map = dict()
 for row in csv:
+	# read data from row
 	timestamp = float(row[0])
 	category = row[6]
-	x = float(row[4])
-	y = float(row[5])
+	latitude = float(row[4])
+	longitude = float(row[5])
 
-	if (not category in category_map):
-		category_map[category] = len(category_map.keys())
-	Labels.append(category_map[category])
-	Features.append([timestamp, x, y])
+	# label mapping
+	if (not category in label_map):
+		label_map[category] = len(label_map.keys())
+
+	# discretize timestamp
+	date = datetime.datetime.fromtimestamp(timestamp)
+	Labels.append(label_map[category])
+	Features.append([date.year, date.month, date.day, date.hour, date.weekday(),\
+					 latitude, longitude])
+
 csvfile.close()
 Labels = np.array(Labels)
 Features = np.array(Features)
 
-# Save into mat format
-sio.savemat('space_time_label.mat', {'Features': Features, 'Labels': Labels})
+# Perform normalization
+(numData, numFeature) = Features.shape
+mins = Features.min(axis=0)
+Features = Features - mins
+maxs = Features.max(axis=0)
+Features = Features / maxs
+
+# save nomralization parameters
+sio.savemat('data/normalizatoin_params', {'minYear': mins[0], 'minMonth': mins[1],\
+										  'minDay': mins[2], 'minHour': mins[3],\
+										  'minWeekday': mins[4], 'minLat': mins[5],\
+										  'minLong': mins[6]})
+
+# Save processed data mat format
+sio.savemat('data/space_time_label.mat', {'Features': Features, 'Labels': Labels})
+
+# Save label map into JSON
+mapfile = open('data/label_map.json', 'w+')
+json.dump(label_map, mapfile)
+mapfile.close()
